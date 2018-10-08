e140aaa60992e28cfa98f22efd7d66d7bf8cfd83
hp0912 <809211365@qq.com>
2018-10-08 21:50:40 +0800

    规范

--- WindowsFormsApp1/Properties/Settings.Designer.cs
+++ WindowsFormsApp1/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WindowsFormsApp1.Properties {
+namespace ScreenTools.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
