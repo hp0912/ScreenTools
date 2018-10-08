53b050b0e89a9c0aeb2802befe9df257f47e7f37
hp0912 <809211365@qq.com>
2018-10-08 20:22:40 +0800

    初始化项目

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+using CefSharp.WinForms;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.语言设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.中文ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.EnglishToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.平台总览ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.产线一览ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.产线监控ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.视频监控ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.检测内核ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.截屏ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.截取当前窗口ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.截取当前屏幕ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.截屏时隐藏当前窗口ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.截取选择部分ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(0, 27);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1109, 560);
+            this.webBrowser1.TabIndex = 6;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.语言设置ToolStripMenuItem,
+            this.平台总览ToolStripMenuItem,
+            this.产线一览ToolStripMenuItem,
+            this.产线监控ToolStripMenuItem,
+            this.视频监控ToolStripMenuItem,
+            this.检测内核ToolStripMenuItem,
+            this.截屏ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1109, 26);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 语言设置ToolStripMenuItem
+            // 
+            this.语言设置ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.中文ToolStripMenuItem,
+            this.EnglishToolStripMenuItem});
+            this.语言设置ToolStripMenuItem.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.语言设置ToolStripMenuItem.MergeIndex = 0;
+            this.语言设置ToolStripMenuItem.Name = "语言设置ToolStripMenuItem";
+            this.语言设置ToolStripMenuItem.Padding = new System.Windows.Forms.Padding(3);
+            this.语言设置ToolStripMenuItem.Size = new System.Drawing.Size(63, 22);
+            this.语言设置ToolStripMenuItem.Text = "语言设置";
+            // 
+            // 中文ToolStripMenuItem
+            // 
+            this.中文ToolStripMenuItem.Name = "中文ToolStripMenuItem";
+            this.中文ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.中文ToolStripMenuItem.Text = "中文";
+            this.中文ToolStripMenuItem.Click += new System.EventHandler(this.中文ToolStripMenuItem_Click);
+            // 
+            // EnglishToolStripMenuItem
+            // 
+            this.EnglishToolStripMenuItem.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.EnglishToolStripMenuItem.Name = "EnglishToolStripMenuItem";
+            this.EnglishToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.EnglishToolStripMenuItem.Text = "English";
+            this.EnglishToolStripMenuItem.Click += new System.EventHandler(this.EnglishToolStripMenuItem_Click);
+            // 
+            // 平台总览ToolStripMenuItem
+            // 
+            this.平台总览ToolStripMenuItem.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.平台总览ToolStripMenuItem.Name = "平台总览ToolStripMenuItem";
+            this.平台总览ToolStripMenuItem.Size = new System.Drawing.Size(65, 22);
+            this.平台总览ToolStripMenuItem.Text = "平台总览";
+            this.平台总览ToolStripMenuItem.Click += new System.EventHandler(this.平台总览ToolStripMenuItem_Click);
+            // 
+            // 产线一览ToolStripMenuItem
+            // 
+            this.产线一览ToolStripMenuItem.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.产线一览ToolStripMenuItem.Name = "产线一览ToolStripMenuItem";
+            this.产线一览ToolStripMenuItem.Size = new System.Drawing.Size(65, 22);
+            this.产线一览ToolStripMenuItem.Text = "产线一览";
+            this.产线一览ToolStripMenuItem.Click += new System.EventHandler(this.产线一览ToolStripMenuItem_Click);
+            // 
+            // 产线监控ToolStripMenuItem
+            // 
+            this.产线监控ToolStripMenuItem.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.产线监控ToolStripMenuItem.Name = "产线监控ToolStripMenuItem";
+            this.产线监控ToolStripMenuItem.Size = new System.Drawing.Size(65, 22);
+            this.产线监控ToolStripMenuItem.Text = "产线监控";
+            // 
+            // 视频监控ToolStripMenuItem
+            // 
+            this.视频监控ToolStripMenuItem.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.视频监控ToolStripMenuItem.Name = "视频监控ToolStripMenuItem";
+            this.视频监控ToolStripMenuItem.Size = new System.Drawing.Size(65, 22);
+            this.视频监控ToolStripMenuItem.Text = "视频监控";
+            // 
+            // 检测内核ToolStripMenuItem
+            // 
+            this.检测内核ToolStripMenuItem.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.检测内核ToolStripMenuItem.Name = "检测内核ToolStripMenuItem";
+            this.检测内核ToolStripMenuItem.Size = new System.Drawing.Size(65, 22);
+            this.检测内核ToolStripMenuItem.Text = "检测内核";
+            this.检测内核ToolStripMenuItem.Click += new System.EventHandler(this.检测内核ToolStripMenuItem_Click);
+            // 
+            // 截屏ToolStripMenuItem
+            // 
+            this.截屏ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.截取当前窗口ToolStripMenuItem,
+            this.截取当前屏幕ToolStripMenuItem,
+            this.截取选择部分ToolStripMenuItem,
+            this.截屏时隐藏当前窗口ToolStripMenuItem});
+            this.截屏ToolStripMenuItem.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.截屏ToolStripMenuItem.Name = "截屏ToolStripMenuItem";
+            this.截屏ToolStripMenuItem.Size = new System.Drawing.Size(41, 22);
+            this.截屏ToolStripMenuItem.Text = "截屏";
+            // 
+            // 截取当前窗口ToolStripMenuItem
+            // 
+            this.截取当前窗口ToolStripMenuItem.Name = "截取当前窗口ToolStripMenuItem";
+            this.截取当前窗口ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.截取当前窗口ToolStripMenuItem.Text = "截取当前窗口";
+            this.截取当前窗口ToolStripMenuItem.Click += new System.EventHandler(this.截取当前窗口ToolStripMenuItem_Click);
+            // 
+            // 截取当前屏幕ToolStripMenuItem
+            // 
+            this.截取当前屏幕ToolStripMenuItem.Name = "截取当前屏幕ToolStripMenuItem";
+            this.截取当前屏幕ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.截取当前屏幕ToolStripMenuItem.Text = "截取当前屏幕";
+            this.截取当前屏幕ToolStripMenuItem.Click += new System.EventHandler(this.截取当前屏幕ToolStripMenuItem_Click);
+            // 
+            // 截屏时隐藏当前窗口ToolStripMenuItem
+            // 
+            this.截屏时隐藏当前窗口ToolStripMenuItem.CheckOnClick = true;
+            this.截屏时隐藏当前窗口ToolStripMenuItem.Name = "截屏时隐藏当前窗口ToolStripMenuItem";
+            this.截屏时隐藏当前窗口ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.截屏时隐藏当前窗口ToolStripMenuItem.Text = "截屏时隐藏当前窗口";
+            // 
+            // 截取选择部分ToolStripMenuItem
+            // 
+            this.截取选择部分ToolStripMenuItem.Name = "截取选择部分ToolStripMenuItem";
+            this.截取选择部分ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.截取选择部分ToolStripMenuItem.Text = "截取选择部分";
+            this.截取选择部分ToolStripMenuItem.Click += new System.EventHandler(this.截取选择部分ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.ClientSize = new System.Drawing.Size(1109, 586);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Bepsun监控系统";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem 语言设置ToolStripMenuItem;
+        private ToolStripMenuItem 中文ToolStripMenuItem;
+        private ToolStripMenuItem EnglishToolStripMenuItem;
+        private ToolStripMenuItem 平台总览ToolStripMenuItem;
+        private ToolStripMenuItem 产线一览ToolStripMenuItem;
+        private ToolStripMenuItem 产线监控ToolStripMenuItem;
+        private ToolStripMenuItem 视频监控ToolStripMenuItem;
+        private ToolStripMenuItem 检测内核ToolStripMenuItem;
+        private ToolStripMenuItem 截屏ToolStripMenuItem;
+        private ToolStripMenuItem 截取当前窗口ToolStripMenuItem;
+        private ToolStripMenuItem 截取当前屏幕ToolStripMenuItem;
+        private ToolStripMenuItem 截屏时隐藏当前窗口ToolStripMenuItem;
+        private ToolStripMenuItem 截取选择部分ToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace WindowsFormsApp1
+{
+
+    public partial class Form1 : Form
+    {
+        public ChromiumWebBrowser browser;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {           
+
+        }
+                      
+        public void InitBrowser(String path)
+        {
+            CefSettings settings = new CefSettings();
+            settings.Locale = "zh-CN";
+            settings.AcceptLanguageList = "zh-CN";
+            Cef.Initialize(settings);
+            browser = new ChromiumWebBrowser(path);
+            browser.Dock = DockStyle.Fill;
+            this.webBrowser1.Controls.Add(browser);
+        }
+
+        private void 中文ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.语言设置ToolStripMenuItem.Text = "语言设置";
+            this.平台总览ToolStripMenuItem.Text = "平台总览";
+            this.产线一览ToolStripMenuItem.Text = "产线一览";
+            this.产线监控ToolStripMenuItem.Text = "产线监控";
+            this.视频监控ToolStripMenuItem.Text = "视频监控";
+
+        }
+
+        private void EnglishToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.语言设置ToolStripMenuItem.Text = "Language Settings";
+            this.平台总览ToolStripMenuItem.Text = "Platform Overview";
+            this.产线一览ToolStripMenuItem.Text = "Production Line List";
+            this.产线监控ToolStripMenuItem.Text = "Production Line Monitoring";
+            this.视频监控ToolStripMenuItem.Text = "Video Surveillance";                      
+        }
+
+        private void 平台总览ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (browser == null)
+            { 
+                InitBrowser("https://www.uccp520.com/bibcor-byitem/uil/cor/byitem/coiall.vm?stm=1110000_1@0");
+            }
+            else
+            {
+                browser.Load("https://www.uccp520.com/bibcor-byitem/uil/cor/byitem/coiall.vm?stm=1110000_1@0");
+            }
+            //browser.Load("http://www.baidu.com");
+        }
+
+        private void 产线一览ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (browser == null)
+            {
+                InitBrowser("https://www.uccp520.com/bibbam-res/uil/bam/res/line/balinall.vm?stm=4110000_1@0");
+            }
+            else
+            {
+                browser.Load("https://www.uccp520.com/bibbam-res/uil/bam/res/line/balinall.vm?stm=4110000_1@0");
+            }
+        }
+
+        private void 检测内核ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (browser == null)
+            {
+                InitBrowser("chrome://extensions/");
+            }
+            else
+            {
+                browser.Load("https://ie.icoa.cn/");
+            }
+            //browser.Load("https://ie.icoa.cn/");
+            //this.webBrowser1.Navigate("https://ie.icoa.cn/");
+        }
+
+        private void 截取当前窗口ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+             ScreenShot.getScreen(this.Location.X, this.Location.Y, this.Size.Width, this.Size.Height, Properties.Settings.Default.ScreenShotPath + DateTime.Now.ToFileTime().ToString() + ".jpg");
+            
+        }
+
+        private void 截取当前屏幕ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.截屏时隐藏当前窗口ToolStripMenuItem.Checked)
+            {
+                this.Hide();
+                Thread.Sleep(1000);
+                ScreenShot.getScreen(0, 0, -1, -1, Properties.Settings.Default.ScreenShotPath + DateTime.Now.ToFileTime().ToString() + ".jpg");
+                this.Show();
+            }
+            else
+            {
+                ScreenShot.getScreen(0, 0, -1, -1, Properties.Settings.Default.ScreenShotPath + DateTime.Now.ToFileTime().ToString() + ".jpg");
+            }
+            
+        }
+
+        private void 截取选择部分ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
